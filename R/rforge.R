f0479caa5b2e71aec90a2168e063c308c5c9177e
Jeroen Ooms <jeroenooms@gmail.com>
2022-02-17 21:26:45 +0100

    Wording

--- R/rforge.R
+++ R/rforge.R
@@ -81,7 +81,7 @@ project_need_update <- function(project){
   cat(sprintf("SVN revision for '%s' is %s\n", project, rev))
   if(as.character(rev) == "0"){
     try(delete_mirror(project))
-    stop("This project seems empty: ", project)
+    stop("This project seems unavailable or empty: ", project)
   }
   if(identical(Sys.getenv('FORCEFRESH'), 'true')){
     return(TRUE)
